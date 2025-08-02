85716d4c3bb50d6660117760545c99e101a03c49
saintbassanaga <saintbassanaga01@gmail.com>
2025-08-02 20:51:12 +0100

    chore: Add class-level Javadoc to application entry points

--- app/src/main/java/tech/saintbassanaga/app/AppApplication.java
+++ app/src/main/java/tech/saintbassanaga/app/AppApplication.java
@@ -3,11 +3,16 @@ package tech.saintbassanaga.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author saintbassanaga
+ * @date 6/26/25
+ * @project AuthServer
+ * @file AppApplication
+ * @description [Insert description here]
+ */
 @SpringBootApplication
 public class AppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
-
 }
--- auth-server/src/main/java/tech/saintbassanaga/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/tech/saintbassanaga/authserver/AuthServerApplication.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import tech.saintbassanaga.authserver.config.RsaKeyProperties;
 
-
+/**
+ * @author saintbassanaga
+ * @date 6/26/25
+ * @project AuthServer
+ * @file AuthServerApplication
+ * @description [Insert description here]
+ */
 @SpringBootApplication
-@EnableConfigurationProperties(RsaKeyProperties.class)
+@EnableConfigurationProperties({RsaKeyProperties.class})
 public class AuthServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
     }
-
 }
-
